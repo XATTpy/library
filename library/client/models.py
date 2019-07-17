2c6f4c0b89a141b20842216feda0cd95ec2f9315
XATTpy <zoomerrr@mail.ru>
2019-07-18 00:19:58 +0300

    add serializers; models: add '__str__'; views: UserView: add serializer

--- library/client/models.py
+++ library/client/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     summary = models.TextField()
     owner = models.ForeignKey('User', related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- library/client/views.py
+++ library/client/views.py
@@ -2,9 +2,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import User
+from .serializers import UserSerializer
 
 
 class UserView(APIView):
     def get(self, request):
         users = User.objects.all()
-        return Response({'users': users})
+        serializer = UserSerializer(users, many=True)
+        return Response({'users': serializer.data})
