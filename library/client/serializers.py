7dc76568868f23ff115bacbfd02bf8468abda221
XATTpy <zoomerrr@mail.ru>
2019-07-18 01:53:37 +0300

    views: method post (UserView); serializers: method create (UserSerializer)

--- library/client/serializers.py
+++ library/client/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
 
+from .models import User
+
 
 class UserSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
+
+    def create(self, varidated_data):
+        return User.objects.create(**varidated_data)
--- library/client/views.py
+++ library/client/views.py
@@ -10,3 +10,10 @@ class UserView(APIView):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response({'users': serializer.data})
+
+    def post(self, request):
+        name = request.data.get('nane')
+        serializer = UserSerializer(data=name)
+        if serializer.is_valid(raise_exception=True):
+            user_saved = serializer.save()
+        return Response({'success': 'User "{}" created successfully'.format(user_saved.name)})
