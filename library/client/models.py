8f1040595526575b3fd709610a0b70a1cf94ecda
XATTpy <zoomerrr@mail.ru>
2019-07-17 23:45:45 +0300

    new classes: User and Book

--- library/client/models.py
+++ library/client/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    summary = models.TextField()
+    owner = models.ForeignKey('User', related_name='books', on_delete=models.CASCADE)
